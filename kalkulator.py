6b4a451181ee0816b27f5a2a68444b708be5e894
Aelita4 <mikorosa02@gmail.com>
2021-01-05 22:13:36 +0100

    Kalkulator 2.0 & gitignore

---
+++ kalkulator.py
@@ -0,0 +1,16 @@
+print("Le kalkulator")
+one = int(input("Działanie: "))
+oper = input()
+two = int(input())
+
+if oper == "/" and two == 0:
+    print("Nie dziel cholero przez zero")
+else:
+    if oper == "+":
+        print(str(one) + oper + str(two) + "=" + str(one + two))
+    elif oper == "-":
+        print(str(one) + oper + str(two) + "=" + str(one - two))
+    elif oper == "*":
+        print(str(one) + oper + str(two) + "=" + str(one * two))
+    elif oper == "/":
+        print(str(one) + oper + str(two) + "=" + str(one / two))
