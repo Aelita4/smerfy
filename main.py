9f765109720041d75ccc7250308373dc6dc73e58
Aelita4 <mikorosa02@gmail.com>
2021-01-05 22:10:10 +0100

    Kalkulator

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import math
 
+print("Le kalkulator")
+one = int(input("Działanie: "))
+oper = input()
+two = int(input())
 
-print("Hello World")
-
-
-
-
-
+if oper == "/" and two == 0:
+    print("Nie dziel cholero przez zero")
+else:
+    if oper == "+":
+        print(str(one) + oper + str(two) + "=" + str(one + two))
+    elif oper == "-":
+        print(str(one) + oper + str(two) + "=" + str(one - two))
+    elif oper == "*":
+        print(str(one) + oper + str(two) + "=" + str(one * two))
+    elif oper == "/":
+        print(str(one) + oper + str(two) + "=" + str(one / two))
