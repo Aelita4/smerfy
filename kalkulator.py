4aa90c3bd888b4a965d9d6d281068b9bc80e1416
Kirukaii <gamerpros450@gmail.com>
2021-01-22 15:27:42 +0100

    LOOPING!

--- kalkulator.py
+++ kalkulator.py
@@ -1,30 +1,43 @@
+
+
 print("Le kalkulator")
 
-one = input("Działanie: \n")
+while True:
 
-try:
-    t = int(one)
-except ValueError:
-    print("To nie liczba")
+    one = input("Działanie: \n")
 
-oper = input()
-two = input()
+    try:
+        t = int(one)
+    except ValueError:
+        print("To nie liczba")
+        continue
 
-try:
-    t2 = int(two)
-except ValueError:
-    print("To nie liczba")
+    oper = input()
+    if oper != "+" or "-"  or "*" or "/":
+        continue
 
-if oper == "/" and t2 == 0:
-    print("Nie dziel cholero przez zero")
-else:
-    if oper == "+":
-        print(str(t) + oper + str(t2) + "=" + str(t + t2))
-    elif oper == "-":
-        print(str(t) + oper + str(t2) + "=" + str(t - t2))
-    elif oper == "*":
-        print(str(t) + oper + str(t2) + "=" + str(t * t2))
-    elif oper == "/":
-        print(str(t) + oper + str(t2) + "=" + str(t / t2))
+    two = input()
+
+    try:
+        t2 = int(two)
+    except ValueError:
+        print("To nie liczba")
+        continue
+    continue
+
+
+
+#check section
+    if oper == "/" and t2 == 0:
+        print("Nie dziel cholero przez zero")
     else:
-        print("Nie można wykonać operacji!")
+        if oper == "+":
+            print(str(t) + oper + str(t2) + "=" + str(t + t2))
+        elif oper == "-":
+            print(str(t) + oper + str(t2) + "=" + str(t - t2))
+        elif oper == "*":
+            print(str(t) + oper + str(t2) + "=" + str(t * t2))
+        elif oper == "/":
+            print(str(t) + oper + str(t2) + "=" + str(t / t2))
+        else:
+            print("Nie można wykonać operacji!")
